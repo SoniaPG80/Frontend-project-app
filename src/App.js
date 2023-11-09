6bec2a2360ddfec648479eba69fd831fe8d19107
Sonia <117350588+SoniaPG80@users.noreply.github.com>
2023-11-09 17:20:00 +0100

    Cambios en hojas de estilo
    No veo los estilos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Index from './paginas/Index';
+import Indice from './paginas/Index';
 import Sobremi from './paginas/Sobremi';
 import Contacto from './paginas/Contacto';
 import Servicios from './paginas/Servicios';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Index from './paginas/Index';
 
 function App() {
   return (
